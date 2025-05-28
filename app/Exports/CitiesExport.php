2c9fbe80a5ea02462ad0d744e5a3462352897285
César Silva <127228753+ces-silv@users.noreply.github.com>
2025-05-27 18:08:37 -0600

    Update CitiesExport.php

--- app/Exports/CitiesExport.php
+++ app/Exports/CitiesExport.php
@@ -5,14 +5,16 @@ namespace App\Exports;
 use App\Models\City;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-
+# New Classes FromCollection and WithHeadings
 class CitiesExport implements FromCollection, WithHeadings
 {
+    # Collection use Laravel Collections, such as Model:all()
     public function collection()
     {
         return City::select('name', 'description')->get();
     }
 
+    # Document Headers
     public function headings(): array
     {
         return ['Name', 'Description'];
